2c7c8645388e270c12006a081c153badf17e9f28
iko <wei.lin.9918@gmail.com>
2025-06-15 15:34:36 +0900

    feat: update group id

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
   lineChannelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   lineChannelSecret: process.env.LINE_CHANNEL_SECRET,
   lineUserId: process.env.LINE_USER_ID,
-  lineGroupId: process.env.LINE_GROUP_ID || "C7fb621f48039ecd7814250fec21d1b13",
+  lineGroupId: process.env.LINE_GROUP_ID || "C202c799a1e99316abbf48a5e4bd4a117",
   password: process.env.PASSWORD,
   userId: process.env.USER_ID,
   webhookPort: process.env.WEBHOOK_PORT || "3000",
