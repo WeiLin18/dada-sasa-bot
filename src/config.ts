7129de3c5cfdfe237392eb807c9ad7e450c1b828
iko <wei.lin.9918@gmail.com>
2025-06-20 17:49:24 +0900

    chore: add miss LINE_GROUP_ID in to yml

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
   lineChannelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   lineChannelSecret: process.env.LINE_CHANNEL_SECRET,
   lineUserId: process.env.LINE_USER_ID,
-  lineGroupId: process.env.LINE_GROUP_ID || "C202c799a1e99316abbf48a5e4bd4a117",
+  lineGroupId: process.env.LINE_GROUP_ID,
   // sumida
   password: process.env.PASSWORD,
   userId: process.env.USER_ID,
